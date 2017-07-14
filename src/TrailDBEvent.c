920494c51a1fc977f1493161cb104f37d4dcfc61
Alexander Holyoke <aholyoke@edu.uwaterloo.ca>
2017-07-13 21:07:36 -0400

    Add TrailDBEvent.c

---
+++ src/TrailDBEvent.c
@@ -0,0 +1,49 @@
+#include <traildb.h>
+#include <string.h>
+#include "../include/TrailDBEvent.h"
+
+
+JNIEXPORT jstring JNICALL Java_traildb_TrailDBEvent_getItem(JNIEnv *env, jobject obj, jint index) {
+	jclass cls;
+	jfieldID fid;
+
+	const tdb *db;
+	const tdb_item *items;
+	const char *value;
+	char *tgt_value;
+	uint64_t value_length;
+
+	// Retrieve items pointer
+
+	cls = (*env)->GetObjectClass(env, obj);
+	fid = (*env)->GetFieldID(env, cls, "items", "J");
+	if (fid == NULL) {
+		exit(1);
+	}
+	items = (tdb_item *) (*env)->GetLongField(env, obj, fid);
+
+	// Retrieve db pointer
+
+	fid = (*env)->GetFieldID(env, cls, "db", "J");
+	if (fid == NULL) {
+		exit(1);
+	}
+	db = (tdb *) (*env)->GetLongField(env, obj, fid);
+
+	if (db == NULL || items == NULL) {
+		exit(1);
+	}
+
+	// Get the value of the item
+
+	value = tdb_get_item_value(db, items[index], &value_length);
+
+	// Convert buffer to null-terminated string
+
+	tgt_value = malloc(value_length * sizeof(char) + 1);
+
+	strncpy(tgt_value, value, value_length);
+	tgt_value[value_length] = '\0';
+
+	return (*env)->NewStringUTF(env, tgt_value);
+}
