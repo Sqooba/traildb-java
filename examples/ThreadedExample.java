cc6ae255df26117a6666170fde9074cb964730f2
Alexander Holyoke <aholyoke@edu.uwaterloo.ca>
2017-07-26 23:44:36 -0400

    Added multithreaded example to test thread-safety

---
+++ examples/ThreadedExample.java
@@ -0,0 +1,50 @@
+
+import traildb.*;
+
+import java.io.FileNotFoundException;
+
+class Parallel implements Runnable {
+	TrailDB tdb;
+	int sessionLimit;
+	Parallel(TrailDB tdb, int sessionLimit) {
+		this.tdb = tdb;
+		this.sessionLimit = sessionLimit;
+	}
+	public void run() {
+		System.out.println("Session Limit: " + sessionLimit);
+		TrailDBCursor cursor = tdb.cursorNew();
+		long n = tdb.numTrails();
+		long totalSessions = 0;
+		long totalEvents = 0;
+
+		for (long i = 0; i < n; i++) {
+			TrailDBEvent event;
+			cursor.getTrail(i);
+			event = cursor.next();
+			long prevTime = event.timestamp;
+			long numSessions = 1;
+			long numEvents = 1;
+			while ((event = cursor.next()) != null) {
+				if (event.timestamp - prevTime > sessionLimit)
+					numSessions++;
+				prevTime = event.timestamp;
+				numEvents++;
+			}
+			totalSessions += numSessions;
+			totalEvents += numEvents;
+		}
+		System.out.println("Session Limit: " + sessionLimit + " Trails: " + n + " Sessions: " + totalSessions + " Events: " + totalEvents);
+	}
+}
+
+class ThreadedExample {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		TrailDB tdb = new TrailDB("wikipedia-history-small.tdb");
+
+		for (int i = 0; i < 8; i++) {
+			Thread thread = new Thread(new Parallel(tdb, 60 * (i + 1)), "" + i);
+			thread.start();
+		}
+	}
+}
