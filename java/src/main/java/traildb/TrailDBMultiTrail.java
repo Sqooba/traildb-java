6c3ca47a82eabcbf41a6e41c7c96d4aaa6e18636
Alex Holyoke <alex.holyoke@adroll.com>
2017-08-05 01:07:21 -0400

    Merged TrailDBMultiCursor into TrailDBMultiTrail java level

--- java/src/main/java/traildb/TrailDBMultiTrail.java
+++ java/src/main/java/traildb/TrailDBMultiTrail.java
@@ -1,7 +1,31 @@
 package traildb;
 
-public class TrailDBMultiTrail
-{
+public class TrailDBMultiTrail {
 	public TrailDBTrail event;
 	public long cursorIndex;
+
+	private long cur;
+
+	public TrailDBMultiTrail(TrailDBTrail[] cursors) {
+		init(cursors);
+	}
+
+	private native void init(TrailDBTrail[] cursors);
+
+	public native void free();
+
+	public native void reset();
+
+	public native TrailDBMultiTrail next();
+
+	public native TrailDBMultiTrail[] nextBatch(int maxEvents);
+
+	public native TrailDBMultiTrail peek();
+
+	private static native void initIDs();
+
+	static {
+		System.loadLibrary("TraildbJavaNative");
+		initIDs();
+	}
 }
