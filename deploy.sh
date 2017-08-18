6c5934650d67be1675016ccf6ef45adea051244a
Baptiste Sottas <baptiste@sqooba.io>
2017-08-18 10:17:07 -0400

    🚑 Fix old variable name.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 -r $SSH_USER@185.19.29.18:/upload down/
 jar uf down/traildb.jar down/libtraildbjava.dylib
-source versions.properties; package_cloud push Nennya/traildb-java/java down/traildb.jar --coordinates=io.sqooba:traildb:${POM_VERSION}
+source versions.properties; package_cloud push Nennya/traildb-java/java down/traildb.jar --coordinates=io.sqooba:traildb:${PROJECT_VERSION}
