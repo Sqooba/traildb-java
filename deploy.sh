d0c68a1a1ef2244e7d8778dd2e2b1eeb4155d446
Baptiste Sottas <baptiste@sqooba.io>
2017-08-21 02:18:06 -0400

    Fix deployment phase.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install -qq sshpass
+gem install package_cloud
 sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 -r $SSH_USER@185.19.29.18:/upload down/
 jar uf down/traildb.jar down/libtraildbjava.dylib
 source versions.properties; package_cloud push Nennya/traildb-java/java down/traildb.jar --coordinates=io.sqooba:traildb:${PROJECT_VERSION}
