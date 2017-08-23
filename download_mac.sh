3791481e095118a5160b875c32179d07df665d25
Baptiste Sottas <baptiste@sqooba.io>
2017-08-23 06:57:00 -0400

    🔧 Change name of uploaded lib so it depends on the travis tag.

--- download_mac.sh
+++ download_mac.sh
@@ -3,3 +3,4 @@
 sudo apt-get install -qq sshpass
 gem install package_cloud
 sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 -r $SSH_USER@185.19.29.18:/upload down/
+mv libtraildbjava-$TRAVIS_TAG.dylib libtraildbjava.dylib
