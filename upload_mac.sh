62a99ab5ffee88935e2c6f72ca40c3ba2595b610
Baptiste Sottas <baptiste@sqooba.io>
2017-08-23 11:46:06 -0400

    🔧 Download only required file.

--- upload_mac.sh
+++ upload_mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 jar -xf target/traildb.jar libtraildbjava.dylib
-mv libtraildbjava.dylib libtraildbjava-$TRAVIS_TAG.dylib
-sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 $SSH_USER@185.19.29.18:/upload <<< "put libtraildbjava-$TRAVIS_TAG.dylib"
+mv libtraildbjava.dylib libtraildbjava.dylib-$TRAVIS_TAG
+sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 $SSH_USER@185.19.29.18:/upload <<< "put libtraildbjava.dylib-$TRAVIS_TAG"
