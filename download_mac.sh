f91375b06aa44ae9d18f328ef3fb45283aa971c1
Baptiste Sottas <baptiste@sqooba.io>
2017-08-23 11:21:39 -0400

    🔧 Variable can be expanded in double quotes.

--- download_mac.sh
+++ download_mac.sh
@@ -6,6 +6,6 @@ gem install package_cloud
 # Download the mac library.
 sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 -r $SSH_USER@185.19.29.18:/upload down/
 # Cleanup.
-sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 $SSH_USER@185.19.29.18:/upload <<< $'rm 'libtraildbjava-$TRAVIS_TAG'.dylib'
+sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 $SSH_USER@185.19.29.18:/upload <<< "rm libtraildbjava-$TRAVIS_TAG.dylib"
 # Rename so it can be used by maven.
 mv libtraildbjava-$TRAVIS_TAG.dylib libtraildbjava.dylib
--- upload_mac.sh
+++ upload_mac.sh
@@ -2,4 +2,4 @@
 
 jar -xf target/traildb.jar libtraildbjava.dylib
 mv libtraildbjava.dylib libtraildbjava-$TRAVIS_TAG.dylib
-sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 $SSH_USER@185.19.29.18:/upload <<< $'put 'libtraildbjava-$TRAVIS_TAG'.dylib'
+sshpass -p $SSH_PASS sftp -oStrictHostKeyChecking=no -oPort=2223 $SSH_USER@185.19.29.18:/upload <<< "put libtraildbjava-$TRAVIS_TAG.dylib"
