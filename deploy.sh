762d8e715be90de6f041f23fc2c7beac3fb9111d
Christopher Chiche <christopher@sqooba.io>
2017-08-24 07:45:47 +0200

    🚀 Download artefacts and install only if deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./download_mac.sh
+mvn clean install -DskipTests
 cp .travis.settings.xml $HOME/.m2/settings.xml
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$TRAVIS_TAG
 mvn clean deploy -DSkipTests
